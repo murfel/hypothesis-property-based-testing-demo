e2c10f0680d9db532d214be4d4acc075a56f0464
natasha <murfel@users.noreply.github.com>
2023-10-13 16:10:58 +0100

    add tests for positive values

--- test.py
+++ test.py
@@ -8,6 +8,11 @@ TerminalNodeStrategy = st.builds(Node, NodeValue)
 NodeStrategy = st.deferred(lambda: TreeStrategy | TerminalNodeStrategy)
 TreeStrategy = st.builds(Node, NodeValue, NodeStrategy, NodeStrategy)
 
+PositiveNodeValue = st.integers(1)
+PositiveTerminalNodeStrategy = st.builds(Node, PositiveNodeValue)
+PositiveNodeStrategy = st.deferred(lambda: PositiveTreeStrategy | PositiveTerminalNodeStrategy)
+PositiveTreeStrategy = st.builds(Node, PositiveNodeValue, PositiveNodeStrategy, PositiveNodeStrategy)
+
 
 @given(TreeStrategy)
 def test_positive_height(node: Node) -> None:
@@ -29,3 +34,15 @@ def test_size_le_2_to_the_nth(node: Node) -> None:
 @given(TreeStrategy)
 def test_sum_ge_0(node: Node) -> None:
     assert node_sum(node) >= 0
+
+
+# In a binary tree with positive values in the nodes, the sum of the values is positive
+@given(PositiveTreeStrategy)
+def test_positive_sum(node: Node) -> None:
+    assert node_sum(node) > 0
+
+
+# In a binary tree with positive values in the nodes, the sum of the values is at least the number of nodes
+@given(PositiveTreeStrategy)
+def test_positive_sum_ge_size(node: Node) -> None:
+    assert node_sum(node) >= node_size(node)
